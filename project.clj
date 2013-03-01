850c387a9e40848c94a06f22ce7c13aad6fff345
Franklin Chen <franklinchen@franklinchen.com>
2013-03-01 17:11:28 -0500

    Update Clojure and dependency versions.

--- project.clj
+++ project.clj
@@ -5,8 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :main merry-fxmas.core
   :repositories {"stuart" "http://stuartsierra.com/maven2"}
-  :dependencies [[org.clojure/clojure "1.5.0-alpha5"]]
-  :profiles {:dev {:dependencies [[midje "1.4.0"]
-                                  [com.stuartsierra/lazytest "1.2.3"]]
-                   :plugins [[lein-midje "2.0.3"]]}}
+  :dependencies [[org.clojure/clojure "1.5.0"]]
+  :profiles {:dev {:dependencies [[midje "1.4.0"]]
+                   :plugins [[lein-midje "2.0.4"]]}}
 )
