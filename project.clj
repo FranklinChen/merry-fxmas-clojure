66cf33583cdfdf4ae10b40cd85ed79c04c7af67f
Franklin Chen <franklinchen@franklinchen.com>
2013-12-23 14:30:50 -0500

    Update versions.

--- project.clj
+++ project.clj
@@ -5,6 +5,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :main merry-fxmas.core
   :dependencies [[org.clojure/clojure "1.5.1"]]
-  :profiles {:dev {:dependencies [[midje "1.5.1"]]
-                   :plugins [[lein-midje "3.0.1"]]}}
+  :profiles {:dev {:dependencies [[midje "1.6.0"]]
+                   :plugins [[lein-midje "3.1.3"]]}}
 )
